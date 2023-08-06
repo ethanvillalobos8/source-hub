8bdd276899d4d039a950fee17059b08fffcd2b9a
Ethan <ethan.villalobos8@gmail.com>
2023-08-05 17:31:07 -0700

    Update server to listen on dynamic port for Heroku
    Updated the server to listen on the port number specified in the `PORT` environment variable, which is necessary for deployment on Heroku. This change ensures that the application will function correctly both in the local development environment and in the Heroku environment. The application will fallback to port 3000 when the `PORT` environment variable is not provided, such as when running the application locally.

--- server.js
+++ server.js
@@ -167,4 +167,7 @@ function checkNotAuthenticated(req, res, next) {
     next();
 };
 
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function() {
+    console.log('Server is running on port ' + PORT);
+});
